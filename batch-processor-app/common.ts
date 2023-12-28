3033610f135bb3ae8a1c99a49f80b637e6401d19
Ian Carlson <iancarlson@ians-mbp.lan>
2023-12-27 20:30:04 -0700

    Made the heartbeat async

--- batch-processor-app/common.ts
+++ batch-processor-app/common.ts
@@ -1,9 +1,10 @@
 import path from "path";
 import fs from "fs";
 
+import { promisify } from "util";
+const writeFilePromise = promisify(fs.writeFile);
+
 export async function writeToHeartbeatFile() {
   const heartbeatFile = path.join(path.join(__dirname), "heartbeat.json");
-
-  // todo, make this async
-  fs.writeFileSync(heartbeatFile, JSON.stringify({ heartBeat: Date.now() }));
+  await writeFilePromise(heartbeatFile, JSON.stringify({ heartBeat: Date.now() }));
 }
