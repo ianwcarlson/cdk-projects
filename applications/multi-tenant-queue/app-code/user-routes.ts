81638aad2faff4bdf74651173167e7aba397e896
Ian Carlson <iancarlson@ians-mbp.lan>
2024-01-07 15:48:20 -0700

    Added api spec defs for most endpoints

--- applications/multi-tenant-queue/app-code/user-routes.ts
+++ applications/multi-tenant-queue/app-code/user-routes.ts
@@ -1,16 +1,25 @@
 import { Router, Request, Response } from "express";
+import { RbacRoles, verifyRole } from "./rbac-roles";
 
 const router = Router();
 
-router.post("/", async (req: Request, res: Response) => {
-  console.log("Request payload: " + JSON.stringify(req.body, null, 2));
-  res.sendStatus(200);
-});
+router.post(
+  "/",
+  verifyRole([RbacRoles.Write, RbacRoles.Admin]),
+  async (req: Request, res: Response) => {
+    console.log("Request payload: " + JSON.stringify(req.body, null, 2));
+    res.sendStatus(200);
+  },
+);
 
-router.get("/:userId", async (req: Request, res: Response) => {
-  res.status(200).send({
-    userId: req.params.userId,
-  });
-});
+router.get(
+  "/:userId",
+  verifyRole([RbacRoles.Read, RbacRoles.Write, RbacRoles.Admin]),
+  async (req: Request, res: Response) => {
+    res.status(200).send({
+      userId: req.params.userId,
+    });
+  },
+);
 
 export default router;
